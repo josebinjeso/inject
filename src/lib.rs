224c20556aaf3ff4d6033011771aeb442c8f37fa
josebinjeso <josebinsejo@gmail.com>
2025-06-11 12:56:58 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> 
         
         if proxy_kv_str.len() == 0 {
             console_log!("getting proxy kv from github...");
-            let req = Fetch::Url(Url::parse("https://github.com/josebinjeso/inject/blob/master/src/proxy/ProxyList.json")?);
+            let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/josebinjeso/inject/master/src/proxy/ProxyList.json")?);
             let mut res = req.send().await?;
             if res.status_code() == 200 {
                 proxy_kv_str = res.text().await?.to_string();
